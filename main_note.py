e0234e5e4e83972a8d69041b2302e9bf57171f23
Nishaanth K <58854404+Nishaanth1611@users.noreply.github.com>
2020-10-19 16:26:43 +0530

    Update main_note.py

--- main_note.py
+++ main_note.py
@@ -1,16 +1,15 @@
+import os
 import tkinter 
 import tkinter as tk
-import os	 
-from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
-
 
 class Notepad: 
     root=Tk()
     _file=None
     TextArea = Text(root)
     def __init__(self,**kwargs): 
+        #To intializing basic elements
         MenuBar = Menu(self.root)
         FileMenu = Menu(MenuBar, tearoff=0) 
         EditMenu = Menu(MenuBar, tearoff=0) 
@@ -21,103 +20,84 @@ class Notepad:
         self.root.grid_rowconfigure(0, weight=1) 
         self.root.grid_columnconfigure(0, weight=1) 
         self.TextArea.grid(sticky = N + E + S + W) 
-        FileMenu.add_command(label="New",command=self.newFile)
-         
-        # To open a already existing file 
-        FileMenu.add_command(label="Open", command=self.openFile) 
-        
-        # To save current file 
-        FileMenu.add_command(label="Save", command=self.saveFile)	 
-
-        # To create a line in the dialog		 
+ 
+        # To initialize features of file menu
+        FileMenu.add_command(label="New",command=self.newfile)
+        FileMenu.add_command(label="Open", command=self.openfile)
+        FileMenu.add_command(label="Save", command=self.savefile)
         FileMenu.add_separator()										 
-        FileMenu.add_command(label="Exit", command=self.quitApplication) 
+        FileMenu.add_command(label="Exit", command=self.quit) 
         MenuBar.add_cascade(label="File", menu=FileMenu)	 
         
-        # To give a feature of cut 
-        EditMenu.add_command(label="Cut", command=self.cut)			 
-    
-        # to give a feature of copy	 
-        EditMenu.add_command(label="Copy", command=self.copy)		 
-        
-        # To give a feature of paste 
+        # To initialize features of edit menu
+        EditMenu.add_command(label="Cut", command=self.cut)	 
+        EditMenu.add_command(label="Copy", command=self.copy)
         EditMenu.add_command(label="Paste", command=self.paste)
-
-        EditMenu.add_command(label="Find", command=self.find_window)	 
-        
-        # To give a feature of editing 
+        EditMenu.add_command(label="Find", command=self.find_window)
         MenuBar.add_cascade(label="Edit", menu=EditMenu)	 
-        
-        # To create a feature of description of the notepad 
-        HelpMenu.add_command(label="About Notepad", command=self.showAbout) 
-        MenuBar.add_cascade(label="Help", menu=HelpMenu) 
 
+        # To initialize features of help menu 
+        HelpMenu.add_command(label="Info", command=self.info)
+        HelpMenu.add_command(label="About Developers", command=self.dev)
+        HelpMenu.add_command(label="Contact Us", command=self.contact)
+        MenuBar.add_cascade(label="About", menu=HelpMenu)					 
+        
+        # To initialize features of scrollbar	 
         self.root.config(menu=MenuBar) 
-
-        ScrollBar.pack(side=RIGHT,fill=Y)					 
-        
-        # Scrollbar will adjust automatically according to the content		 
+        ScrollBar.pack(side=RIGHT,fill=Y)
         ScrollBar.config(command=self.TextArea.yview)	 
         self.TextArea.config(yscrollcommand=ScrollBar.set) 
     
-        
-    def quitApplication(self): 
-        self.root.destroy() 
-        # exit() 
+    def run(self): 
+        self.root.mainloop()    
+    
+    def quit(self): 
+        self.root.destroy()
 
-    def showAbout(self): 
-        showinfo("Notepad","This notepad is developed by Nishaanth K and Heflin Stephen Raj S") 
+    def info(self): 
+        showinfo("Info","This a Simple Notepad made from Python Tkinter.\nThis contain many scratchly made functions like New, Open, Save, Cut, Copy, Paste and Find.")
 
-    def openFile(self): 
-        
-        self._file = askopenfilename(defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")]) 
+    def dev(self): 
+        showinfo("About Developers","This notepad is developed by Nishaanth K and Heflin Stephen Raj S")
 
+    def contact(self): 
+        showinfo("Contact Us","Contact Us:\nNishaanth K : https://www.linkedin.com/in/nishaanth-k \nHeflin Stephen Raj S : https://www.linkedin.com/in/heflin-stephen-raj/ ")
+
+    def newfile(self): 
+        self.root.title("Untitled - Notepad") 
+        self._file = None
+        self.TextArea.delete(1.0,END)
+
+    def openfile(self):
+        self._file = askopenfilename(defaultextension=".txt", filetypes=[("Text Documents","*.txt"),("All Files","*.*")]) 
         if self._file == "": 
-            
             # no file to open 
             self._file = None
         else: 
-            
-            # Try to open the file 
-            # set the window title 
+            # Try to open the file
             self.root.title(os.path.basename(self._file) + " - Notepad") 
             self.TextArea.delete(1.0,END) 
-
             file = open(self._file,"r") 
-
             self.TextArea.insert(1.0,file.read()) 
-
             file.close() 
-        
-        
-    def newFile(self): 
-        self.root.title("Untitled - Notepad") 
-        self._file = None
-        self.TextArea.delete(1.0,END) 
 
-    def saveFile(self): 
-
+    def savefile(self): 
         if self._file == None: 
             # Save as new file 
-            self._file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files","*.*"), ("Text Documents","*.txt")]) 
-
+            self._file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("Text Documents","*.txt"),("All Files","*.*")]) 
             if self._file == "": 
                 self._file = None
             else: 
-                
                 # Try to save the file 
                 file = open(self._file,"w") 
                 file.write(self.TextArea.get(1.0,END)) 
-                file.close() 
-                
-                # Change the window title 
+                file.close()                
                 self.root.title(os.path.basename(self._file) + " - Notepad") 
-                
-            
+                         
         else: 
             file = open(self._file,"w") 
             file.write(self.TextArea.get(1.0,END)) 
-            file.close() 
+            file.close()
 
     def find_window(self):
         app=tk.Tk()
@@ -126,22 +106,17 @@ class Notepad:
         info = Label(app,text='Enter the text to find: ')
         edit = Entry(app)  
         edit.focus_set()  
-        btm = Button(app, text='Find')
+        btn = Button(app, text='Find')
         close = Button(app, text = "Close")
-        close["command"] = lambda text= self.TextArea, app = app : self.clear(text,app) 
-        btm["command"] = lambda text_to_search=edit, app= self.TextArea : self.find(app, text_to_search) 
-        app.protocol("WM_DELETE_WINDOW", lambda text= self.TextArea, app = app : self.clear(text,app))
+        close["command"] = lambda text= self.TextArea, app = app : self.find_clear(text,app) 
+        btn["command"] = lambda text_to_search=edit, app= self.TextArea : self.find(app, text_to_search) 
+        app.protocol("WM_DELETE_WINDOW", lambda text= self.TextArea, app = app : self.find_clear(text,app))
         info.grid(row=0,column=0)
         edit.grid(row=0,column=1)  
-        btm.grid(row=2,column=0)  
+        btn.grid(row=2,column=0)  
         close.grid(row=2,column=1)
-
-
+        
         app.mainloop()
-
-    def clear(self,text, app):
-        text.tag_remove('found', '1.0', END)
-        app.destroy()
     
     def find(self,text,app): 
         text.tag_remove('found', '1.0', END)  
@@ -157,6 +132,9 @@ class Notepad:
         text.tag_config('found', foreground='white',background="black")  
         app.focus_set() 
 
+    def find_clear(self,text, app):
+        text.tag_remove('found', '1.0', END)
+        app.destroy()
       
     def cut(self): 
         self.TextArea.event_generate("<<Cut>>") 
@@ -166,18 +144,6 @@ class Notepad:
 
     def paste(self): 
         self.TextArea.event_generate("<<Paste>>")
-
-    def search(self):
-        
-        dir_path = askdirectory()
-        for root, files in os.walk(dir_path): 
-            for file in files:  
-                 if file.endswith('.txt'): 
-                     print (root+'/'+str(file))
-    
-    def run(self): 
-        self.root.mainloop()
-
 
 notepad = Notepad()
 notepad.run()
